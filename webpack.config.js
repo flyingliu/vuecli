94ed9c0df0f35439140363990c291a1cea51f242
flying <fly0733@gmail.com>
2017-04-04 23:05:22 +0800

    a

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: '/vuecli/dist/',
         filename: 'build.js'
     },
     module: {
